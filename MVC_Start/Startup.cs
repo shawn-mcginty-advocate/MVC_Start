16c2711ef4278bdd06358adb7ca80e74b9aaf994
subsonar704 McGinty <spmcginty1030@gmail.com>
2020-07-05 20:16:31 -0400

    Link DemoChart to Home Page

--- MVC_Start/Startup.cs
+++ MVC_Start/Startup.cs
@@ -53,7 +53,7 @@ namespace MVC_Start
       {
         routes.MapRoute(
             name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
+            template: "{controller=Home}/{action=DemoChart}/{id?}");
       });
     }
   }
